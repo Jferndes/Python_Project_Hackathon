21709997658748649df15dd3e912746077111778
Jferndes <122863374+Jferndes@users.noreply.github.com>
2023-12-11 14:07:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,6 @@ app.secret_key = 'test'
 def index():
     return render_template('index.html')
 
-@app.get('/transform/<word>')
-def transform(word: str):
-    return render_template('transform.html', reverseWord = word[::-1])
-
-
-@app.get('/calcul/<HT>')
-def calcul(HT: float):
-    val = int(HT) * (1 + 1/5)
-    return render_template('calcul.html', valeur=val)
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
