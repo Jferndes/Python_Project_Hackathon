4a83d8ba37390e6acf6c1233d9c9ea2a1263b3a0
Jferndes <122863374+Jferndes@users.noreply.github.com>
2023-12-12 15:54:54 +0100

    Ajout route ajouter contact et tout ce qui suit Modification de la structure des tables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'test'
@@ -40,6 +41,34 @@ def liste_groupes():
 
     # Rendu du modèle avec les données récupérées
     return render_template('liste_groupes.html', groupes=groupes)
+
+
+@app.route("/ajouter_contact",  methods=['POST', 'GET'])
+def ajouter_contact():
+    if request.method == 'POST':
+        #Récup info form
+        nom = request.form['nom']
+        prenom = request.form['prenom']
+        email = request.form['email']
+        tel = request.form['tel']
+        date_naissance = request.form['dob']
+        photo = request.form['photo']
+
+        print(nom, prenom, email, tel, date_naissance, photo)
+        # Connexion à la base de données (cela créera la base de données si elle n'existe pas encore)
+        connection = connect_db()
+        cursor = connection.cursor()
+
+        cursor.execute('''
+                INSERT INTO Contact (nom, prenom, e_mail, tel, date_naissance, photo_profil, created_date, updated_date)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (nom, prenom, email, tel, date_naissance, photo, datetime.now(), datetime.now()))
+
+        connection.commit()
+        connection.close()
+
+    return render_template('ajouter.html')
+
 
 @app.get("/")
 def index():
