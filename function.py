0debc462f93e28176740c675b99f4df7e1096c63
Jferndes <122863374+Jferndes@users.noreply.github.com>
2023-12-14 16:39:37 +0100

    Grosse refonte du code + Ajout update + Ajout delete

---
+++ function.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+# Fonction pour se connecter à la base de données
+def connect_db():
+    return sqlite3.connect('hackathon_bdd.sqlite')
+
+# Fonction pour pour récupérer des données dans base de données
+def recup_bdd(query: str, doOne: bool = False):
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+    if doOne:
+        result = cursor.fetchone()
+    else:
+        result = cursor.fetchall()
+
+    connection.close()
+    return result
+
+# Fonction pour faire une action (INSERT, UPDATE, DELETE) dans base de données
+def action_bdd(query: str):
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
